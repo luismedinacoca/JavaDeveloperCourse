f02fe9f0ac2bff5c3fe59bce0c71053e812513c9
Louis <luismedinacoca@gmail.com>
2019-12-03 18:08:44 -0300

    Section03_lecture021

--- Section03/src/java/HelloWorld.java
+++ Section03/src/java/HelloWorld.java
@@ -1,21 +1,9 @@
 package java;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Luis");
+    }
 }
-/**
- * DEFINING THE MAIN METHOD
- *
- * What is a method?
- * it's a collection of statements (one or more) that perform a operation
- * We'll be using a special method call the main method that java looks for when running a program.
- *
- * public static void main (String[] args){
- *
- * }
- *
- * psvm + tab
- *
- * public: done in last lecture
- * static:  <we need more information later>
- * void: it will not return anything
- */
+
+
