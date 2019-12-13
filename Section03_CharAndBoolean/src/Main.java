b381914c17c1c5324f62a3113ebf13ce7212a7b4
Louis <luismedinacoca@gmail.com>
2019-12-13 16:49:10 -0300

    Section 03 - Conditionals part01 - 'til lecture 39

--- Section03_CharAndBoolean/src/Main.java
+++ Section03_CharAndBoolean/src/Main.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        /** always in simple quotes and accept just only one character*/
+        // char myChar = 'LM'    ===> there will be an error
+        // char myChar = "L"     ===> there will be an error
+        char myChar = 'L'; //unicode: 0040C
+        char myUnicode = '\u004C';
+        System.out.println("My char is: " + myChar);
+        System.out.println("My unicode is: " + myUnicode);
+        // look for unicode-table.com
+        char myCopyrightChar = '\u00A9';
+        System.out.println("My copyright char is: " + myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+        /**
+         * We have seen byte, short, int, long, float, double, char and boolean and also STRING
+         */
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to "+ myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to "+ myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to "+ myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("There is not any calculation " + numberString);
+        int myInt = 50;
+        String lastString = "10";
+        //lastString = lastString + myInt;
+        System.out.println("myInt is equal to " + myInt);
+        System.out.println("LastString is equal to " + lastString);
+        System.out.println( lastString + myInt);
+        System.out.println(myInt + lastString);
+
+        int result = 1 + 2; //1 + 2 = 3
+        /**
+         * '=' assignment operator
+         * '+' sum operator
+         */
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1; //3 - 1 = 2
+        System.out.println("result: 3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+    }
 }
--- Section03_IfThenStatement/src/IfThenStatement.java
+++ Section03_IfThenStatement/src/IfThenStatement.java
@@ -64,6 +64,30 @@ public class IfThenStatement {
         String isEightieeenOrOver = (ageOfClient == 20) ? "S/he is an adult" : "s/he is not an adult";
         System.out.println(isEightieeenOrOver);
 
+        /** LECTURE 39 - Operator Challenge */
+
+        /**
+         1. Create a double variable with a value of 20.00.
+         2. create a second variable of type double with the value 80.00.
+         3. Add both numbers together and multiply by 100.00.
+         4. Use the remainder operator to figure out what the reminder from the result of the operation in steps 3 and 40.00.
+            we used the modulus or remainder operator on int's in the course, but we can also use it on a double.
+         5. create a boolean variable that assign the value true if the remainder is #4 is 0, or false if its not zero.
+         6. Output the boolean variable.
+         7. Write an if-then statement that displays a message "Got some remainder" if the boolean is step 5 is not true.
+            Don't be surprised if you see output for this step, where you might expect it not to show. I'll explain it in my solution.*/
+
+        double myDoubleValue = 21;
+        double mySecondDoubleValue = 80;
+        double theDoubleValue = (myDoubleValue + mySecondDoubleValue) * 100;
+        double remainder = theDoubleValue % 40;
+        boolean isZero = (remainder == 0) ? true : false;
+        System.out.println(remainder);
+        System.out.println("The result: " + isZero);
+        if (isZero == false){
+            System.out.println("Got some remainder!");
+        }
+
 
     }
 }
